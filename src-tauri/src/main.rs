289b7194ca23f698708e0a98fb9b85e5a6389d03
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-01 23:59:26 +0900

    fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,18 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io;
+use std::path::Path;
+use std::{env, fs::File};
+use zip::write::{FileOptions, ZipWriter};
+
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info")
+    }
+    pretty_env_logger::init();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello])
+        .invoke_handler(tauri::generate_handler![run, hello])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -11,4 +20,28 @@ fn main() {
 #[tauri::command]
 fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn run(input_paths: Vec<String>, output_path: String) {
+    log::info!(
+        "Running with files: {:?} and output path: {}",
+        input_paths,
+        output_path
+    );
+
+    let path = Path::new(&output_path);
+    let file = File::create(path).unwrap();
+    let mut zip = ZipWriter::new(file);
+
+    for file in input_paths {
+        let path = Path::new(&file);
+        let name = path.file_name().unwrap().to_str().unwrap();
+        let options = FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+        zip.start_file(name, options).unwrap();
+        let mut f = File::open(path).unwrap();
+        io::copy(&mut f, &mut zip).unwrap();
+    }
+
+    zip.finish().unwrap();
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,71 @@
 <script>
+	import { dialog } from '@tauri-apps/api';
 	import { invoke } from '@tauri-apps/api/tauri';
 
-	let name = '';
-	let msg = '';
+	let inputPaths = [];
+	let outputPath = '';
 
-	async function hello() {
-		msg = await invoke('hello', { name });
+	async function openFileDialog() {
+		const res = await dialog.open({
+			multiple: true,
+			directory: false
+		});
+		if (!res) return;
+		inputPaths = Array.isArray(res) ? res : [res];
+	}
+
+	async function openOutputDialog() {
+		const res = await dialog.save({
+			filters: [
+				{
+					name: 'Output format',
+					extensions: ['zip']
+				}
+			]
+		});
+		outputPath = Array.isArray(res) ? res[0] : res;
+	}
+
+	async function compressFiles() {
+		if (!inputPaths) {
+			alert('入力フォルダ/ファイルを選択してください');
+			return;
+		}
+		if (!outputPath) {
+			alert('出力先を選択してください');
+			return;
+		}
+
+		await invoke('run', {
+			inputPaths,
+			outputPath
+		});
+		alert('ファイルの圧縮と出力が完了しました。');
 	}
 </script>
 
 <div class="text-center flex flex-col gap-12 my-12">
-	<h1 class="text-2xl font-bold text-red-700">Hello MIERUNE</h1>
+	<div class="text-center flex flex-col gap-12 my-12">
+		<h1 class="text-2xl font-bold text-red-700">ジップマン</h1>
 
-	<div>
-		<input class="border-4" id="hello-input" placeholder="Enter a name..." bind:value={name} />
-		<button on:click={hello}>Greet</button>
-		<p>{msg}</p>
+		<div>
+			<button on:click={openFileDialog}>入力ファイル選択</button>
+			<button on:click={openOutputDialog}>出力ファイル選択</button>
+		</div>
+		<div>
+			<p>入力ファイル:</p>
+			{#if inputPaths.length > 0}
+				{#each inputPaths as path}
+					<p>{path}</p>
+				{/each}
+			{/if}
+			<p>保存先:</p>
+			{#if outputPath}
+				<p>{outputPath}</p>
+			{/if}
+		</div>
+		<div>
+			<button on:click={compressFiles}>圧縮</button>
+		</div>
 	</div>
 </div>
