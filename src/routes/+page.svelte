9d8b63ab347f0a28cf4dc8b0604c397133d14e35
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-01 20:57:02 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let msg = '';
+
+	async function hello() {
+		msg = await invoke('hello', { name });
+	}
+</script>
+
+<div class="text-center flex flex-col gap-12 my-12">
+	<h1 class="text-2xl font-bold text-red-700">Hello MIERUNE</h1>
+
+	<div>
+		<input class="border-4" id="hello-input" placeholder="Enter a name..." bind:value={name} />
+		<button on:click={hello}>Greet</button>
+		<p>{msg}</p>
+	</div>
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static'; // This was changed from adapter-auto
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
